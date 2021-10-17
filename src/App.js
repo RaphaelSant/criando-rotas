1a9b08dca17dc474273c3ee74508888fb2d2223b
Rafa <raphael.moura.2015@gmail.com>
2021-10-16 21:56:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Rotas from './componentes/Rotas';
+
+const App = () => (
+  <div className="App">
+    <div className="card">
+      <Rotas />
+    </div>
+  </div>
+);
+
+export default App;
